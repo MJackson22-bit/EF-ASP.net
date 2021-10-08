c821d746ea5d42ecbd6ddbaf12c7812d8c14974c
MJackson22-bit <jm5444859@gmail.com>
2021-10-08 00:42:28 -0600

    Subiendo ficheros

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function loadNoticias(id, event){
+    $('.modal').modal('show');
+    event.preventDefault();
+    fetch('https://localhost:5001/Noticias/Index/' + id)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('div_noticias').innerHTML = html;
+    });
+}
+
+$(document).ready(function(){
+    $(".toast").toast('show');
+});
